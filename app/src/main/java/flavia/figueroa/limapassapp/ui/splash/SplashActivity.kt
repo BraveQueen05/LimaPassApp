dfda27190955f1dc8850dc0c9432ddfa5d53ce8f
flaviafigueroa <ffigueroa@solera.pe>
2020-09-09 01:49:41 -0500

    Logo and logo muni implemented

--- app/src/main/java/flavia/figueroa/limapassapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/flavia/figueroa/limapassapp/ui/splash/SplashActivity.kt
@@ -1,11 +1,12 @@
-package flavia.figueroa.limapassapp
+package flavia.figueroa.limapassapp.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import flavia.figueroa.limapassapp.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
