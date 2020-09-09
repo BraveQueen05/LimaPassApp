73daacf7cdaadbc12ae08fc210fa8764c6a94dce
flaviafigueroa <ffigueroa@solera.pe>
2020-09-09 01:14:07 -0500

    Initial commit

---
+++ app/src/main/java/flavia/figueroa/limapassapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package flavia.figueroa.limapassapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
